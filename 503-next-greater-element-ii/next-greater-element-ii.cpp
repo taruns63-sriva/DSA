50364f70e0207c190ac3382a317926f57379654a
taruns63-sriva <taruns750563@gmail.com>
2026-08-03 11:05:06 +0530

    Time: 3 ms (74.38%) | Memory: 28.5 MB (64.82%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n =nums.size();
-        vector<int>ans(n,1);
+        vector<int>ans(n,-1);
         stack<int>s;
         for(int i= 2*n-1;i>=0;i--){
             while(s.size()>0 && nums[s.top()]<=nums[i%n]){
