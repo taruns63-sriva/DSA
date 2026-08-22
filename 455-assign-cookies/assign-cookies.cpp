01891a6296fb15ef71d200fc5df49fcee52d3ca7
taruns63-sriva <taruns750563@gmail.com>
2026-08-23 00:04:41 +0530

    Time: 11 ms (39.47%) | Memory: 44.7 MB (97.58%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n= s.size();
+        int m= g.size();
+
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+
+        int i=0, j=0;
+        int cnt=0;
+
+        while(i<n && j<m){
+            if(s[i]>=g[j]){
+                cnt++;
+                i++;
+                j++;
+            } 
+            else if(s[i]<g[j]){
+                i++;
+            } 
+            else {
+                j--;
+            }
+        }
+        return cnt;
+    }
+};
